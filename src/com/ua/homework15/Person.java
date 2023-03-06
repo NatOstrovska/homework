31dc425a42240652efe5bd54117dcd81b7e8ae95
Nataliia O <natali.o.1704@gmail.com>
2023-03-06 23:57:35 +0200

    homework 15 is added.

---
+++ src/com/ua/homework15/Person.java
@@ -0,0 +1,76 @@
+package com.ua.homework15;
+
+public class Person {
+    String name;
+    String surname;
+    String id;
+    int age;
+    String sex;
+    String education;
+    String profession;
+    String workExperience;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getWorkExperience() {
+        return workExperience;
+    }
+
+    public void setWorkExperience(String workExperience) {
+        this.workExperience = workExperience;
+    }
+}
